7825297fcd4f596e6933e78d2705f809ce41dfbb
Iago Estrela <IagoFilipe.EstrelaBarros@windriver.com>
2021-08-23 12:40:50 -0300

    convert_int_to_str

---
+++ cast_to_str.asm
@@ -0,0 +1,37 @@
+global _start
+_start:
+    mov eax, 123456
+    call cast_to_str
+
+    mov ecx, esi
+    mov eax, 4
+    mov ebx, 1
+    mov edx, 22
+    int 0x80
+
+    mov eax, 1
+    int 0x80
+
+cast_to_str:
+    push ebp
+    mov ebp, esp
+
+    loop:
+        xor edx, edx
+        mov ebx, 10
+        div ebx
+        add edx, 48
+
+        push edx
+
+        cmp eax, 0
+        jne loop
+
+    mov esi, esp
+
+    mov esp, ebp
+    pop ebp
+    ret
+
+
+; To do: how to recover the IP of the caller function
